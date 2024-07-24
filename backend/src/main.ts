3f1f8cecf4c05a37bfd73021a4da40b94ddfc3f0
Fantazia01 <tothfabian01@gmail.com>
2024-07-24 18:36:13 +0200

    try again v5

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://frontend-quiet-night-5362.fly.dev',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: '*',
+    methods: '*',
     credentials: true,
   });
   await app.listen(3000, '0.0.0.0');
