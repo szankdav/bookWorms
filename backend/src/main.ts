9c51e2d4fd845b86e337e084d69f30f9d5ec51c5
Fantazia01 <tothfabian01@gmail.com>
2024-07-24 18:32:18 +0200

    try aganin v4

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'https://frontend-quiet-night-5362.fly.dev',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
